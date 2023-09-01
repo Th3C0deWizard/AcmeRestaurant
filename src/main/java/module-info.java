7687b1b7a843282f656e55aae5c7e8ecdcbcb392
carlosa paez <carlosa.paezc@autonoma.edu.co>
2023-09-01 17:23:56 -0500

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.j2devs.acmerestaurant {
+  requires javafx.controls;
+  requires javafx.fxml;
+
+  requires org.controlsfx.controls;
+  requires org.kordamp.bootstrapfx.core;
+
+  opens com.j2devs.acmerestaurant to javafx.fxml;
+  exports com.j2devs.acmerestaurant;
+}
